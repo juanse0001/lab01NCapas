03374d526368d5847b378f60839b09ca68467333
Juan Sebastian Ordoñez Sierra <Juanseordo2206@outlook.com>
2024-07-25 12:13:58 -0500

    fix: file conflict

--- Orders/Entities/Models/OrderItem.cs
+++ Orders/Entities/Models/OrderItem.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace Entities.Models;
 
 [Table("OrderItem")]
