43bd84db2c4ca58c94253643cdc19e8d8964a591
Juan Sebastian Ordoñez Sierra <Juanseordo2206@outlook.com>
2024-07-29 12:11:17 -0500

    feat(back-end): Add business logic layer in BLL

---
+++ Orders/BLL/CustomerExceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    public class CustomerExceptions : Exception
+    {
+        // You can add more static methods here to throw other customer-related exceptions
+
+        private CustomerExceptions(string message) : base(message)
+        {
+            // Optional: Add constructor logic for logging or custom error handling
+        }
+
+        public static void ThrowCustomerAlreadyExistsException(string firstName, string lastName)
+        {
+            throw new CustomerExceptions($"A client with the name already exists {firstName} {lastName}.");
+        }
+
+        public static void ThrowInvalidCustomerDataException(string message)
+        {
+            throw new CustomerExceptions(message);
+        }
+    }
+}
