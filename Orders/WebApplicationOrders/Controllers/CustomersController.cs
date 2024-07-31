d566b435fddda0e9824d781e787653f1305874a6
Juan Sebastian Ordoñez Sierra <Juanseordo2206@outlook.com>
2024-07-31 00:11:39 -0500

    feat(back-end) Add model view controller layer WebApplicationOrders

---
+++ Orders/WebApplicationOrders/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Entities.Models;
+using ProxyService.Interfaces;
+using ProxyService;
+
+namespace WebApplicationOrders.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CustomerProxy _proxy;  
+        public CustomersController() 
+        {
+            this._proxy = new CustomerProxy();
+        }        
+        public async Task <IActionResult> Index()
+        {
+            var customers = await _proxy.GetAllAsync();
+            return View(customers);
+        }
+    }
+}
