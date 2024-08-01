609a5276d4500231c9fb6b4f7d6486583de36d30
Juan Sebastian Ordoñez Sena <juanseordo2206@outlook.com>
2024-08-01 17:53:05 -0500

    feat(back-end): Adding views Edit, Error and modifications in CustomerController

--- Orders/WebApplicationOrders/Controllers/CustomersController.cs
+++ Orders/WebApplicationOrders/Controllers/CustomersController.cs
@@ -21,7 +21,7 @@ namespace WebApplicationOrders.Controllers
             return View(customers);
         }
 
-        //Crete 
+        //Create 
         //GET: Customers/Create
         public ActionResult Create()
         {
@@ -31,14 +31,14 @@ namespace WebApplicationOrders.Controllers
         //POST Customer/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Firstmane,LastName,City,Country,Phone")]Customer customer)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,City,Country,Phone")]Customer customer)
         {
             if (ModelState.IsValid) 
             {
                 try
                 {
                     var result = await _proxy.CreateAsync(customer);
-                    if (result != null) 
+                    if (result == null) 
                     {
                         return RedirectToAction("Error", new {message = "El cliente con el mismo nombre y apellido ya existe."});
                     }
@@ -50,6 +50,68 @@ namespace WebApplicationOrders.Controllers
                 }
             }
             return View(customer);
+        }
+
+        //Edit 
+        //GET : Customers/Edit/5
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var customer = await _proxy.GetByIdAsync(Id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        //Details
+        //GET: /Customer/Details/5
+        public async Task<IActionResult> Details(int Id) 
+        {
+            var customers = await _proxy.GetByIdAsync(Id);
+            if(customers == null)
+            {
+                return NotFound();
+            }
+            return View(customers);
+        }
+
+
+        //POST: Customer/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, FirstName, LastName, City, Country, Phone")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await _proxy.UpdateAsync(id, customer);
+                    if (!result)
+                    {
+                        return RedirectToAction("Error", new { message = "No se puede realizar la edición porque hay duplicidad de nombre con otro cliente." });
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    return RedirectToAction("Error", new { message = ex.Message });
+                }
+            }
+            return View(customer);
+        }
+
+
+        //Error
+        public IActionResult Error(string message) 
+        {
+            ViewBag.ErrorMessage = message;
+            return View();
         }
     }
 }
