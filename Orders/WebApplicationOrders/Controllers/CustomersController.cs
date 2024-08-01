9abd53fa2c1ecc14ed73d89d9ea653999846f16d
Juan Sebastian Ordoñez Sierra <Juanseordo2206@outlook.com>
2024-08-01 11:00:40 -0500

    feat(back-end): Add method Create in to CustomersController

--- Orders/WebApplicationOrders/Controllers/CustomersController.cs
+++ Orders/WebApplicationOrders/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using Entities.Models;
 using ProxyService.Interfaces;
 using ProxyService;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 
 namespace WebApplicationOrders.Controllers
 {
@@ -11,17 +12,44 @@ namespace WebApplicationOrders.Controllers
         public CustomersController() 
         {
             this._proxy = new CustomerProxy();
-        }        
+        }
+ 
+        //GET : Customer
         public async Task <IActionResult> Index()
         {
             var customers = await _proxy.GetAllAsync();
             return View(customers);
         }
+
         //Crete 
         //GET: Customers/Create
         public ActionResult Create()
         {
             return View();
+        }
+
+        //POST Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Firstmane,LastName,City,Country,Phone")]Customer customer)
+        {
+            if (ModelState.IsValid) 
+            {
+                try
+                {
+                    var result = await _proxy.CreateAsync(customer);
+                    if (result != null) 
+                    {
+                        return RedirectToAction("Error", new {message = "El cliente con el mismo nombre y apellido ya existe."});
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex) 
+                {
+                    return RedirectToAction("Error", new { message = ex.Message });
+                }
+            }
+            return View(customer);
         }
     }
 }
